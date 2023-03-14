347541d6281b59caf011b4e01907c914d5892487
laetitiavulihwadi <laetitiavulihwadi@gmail.com>
2023-03-14 11:56:54 +0100

    Part three

---
+++ page.js
@@ -0,0 +1,22 @@
+const ci=document.querySelector(".C")
+ci.addEventListener("click", function(){
+    alert("Pour voir nos contacts")
+})
+const mi=document.querySelector(".M")
+mi.addEventListener('click', function(){
+    alert("Comprendre notre mission")
+})
+const ac=document.querySelector(".A")
+ac.addEventListener('click', function(){
+    alert("Pour voir nos activités")
+})
+const vi=document.querySelector(".V")
+vi.addEventListener('click', function(){
+    alert(" Voir notre vision et nos objectis")
+})
+
+const en=document.querySelector(".client")
+en.addEventListener('click', function(){
+    alert("Enregistres-toi")
+})
+document.getElementById()
